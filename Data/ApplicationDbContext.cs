7df26ff8d8ffab065ac40d5a7b73ca09c85cd48d
Danielvi <danielvilopez@gmail.com>
2025-07-21 23:07:47 -0400

    Crea las migraciones necesarias

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace InfalibleRealEstate.Data
+namespace InfalibleRealEstate.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
 }
