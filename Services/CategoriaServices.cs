83c30e662dc538fcd651f5395e9a7cae18f4e9d0
Danielvi <danielvilopez@gmail.com>
2025-07-27 23:47:48 -0400

    Crea y configura el servicio para manejar las Categorias de Propiedades

---
+++ Services/CategoriaServices.cs
@@ -0,0 +1,18 @@
+﻿using InfalibleRealEstate.Data;
+using InfalibleRealEstate.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace InfalibleRealEstate.Services;
+
+public class CategoriaServices(IDbContextFactory<ApplicationDbContext> DbContext)
+{
+    public async Task<List<Categoria>> Listar(Expression<Func<Categoria, bool>> criterio)
+    {
+        await using var contexto = await DbContext.CreateDbContextAsync();
+        return await contexto.Categorias
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
