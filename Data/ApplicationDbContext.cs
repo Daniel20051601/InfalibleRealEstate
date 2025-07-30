649afb16c4b17d6752a06aac3ad422bb628054aa
Danielvi <danielvilopez@gmail.com>
2025-07-30 10:20:00 -0400

    Agrega una restriccion para que no se elimine la propiedad cuando se elimine una categoria y agrega algunas categorias y estados a las tablas en la base de datos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -37,5 +37,27 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         builder.Entity<Propiedad>()
                .Property(p => p.Precio)
                .HasColumnType("decimal(18,2)");
+
+        builder.Entity<Propiedad>()
+        .HasOne(p => p.Categoria)
+        .WithMany(c => c.Propiedades)
+        .HasForeignKey(p => p.CategoriaId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Categoria>().HasData(
+            new Categoria { CategoriaId = 1, NombreCategoria = "Apartamento", Descripcion = "Unidad de vivienda en un edificio de apartamentos." },
+            new Categoria { CategoriaId = 2, NombreCategoria = "Casa", Descripcion = "Vivienda unifamiliar o adosada." },
+            new Categoria { CategoriaId = 3, NombreCategoria = "Terreno", Descripcion = "Lote de tierra disponible para construcción." },
+            new Categoria { CategoriaId = 4, NombreCategoria = "Local Comercial", Descripcion = "Espacio para negocios y oficinas." },
+            new Categoria { CategoriaId = 5, NombreCategoria = "Villa", Descripcion = "Casa de lujo, a menudo con jardín o terreno grande." },
+            new Categoria { CategoriaId = 6, NombreCategoria = "Penthouse", Descripcion = "Apartamento de dos niveles, regularmente con Jacuzzi" }
+        );
+
+        builder.Entity<EstadoPropiedad>().HasData(
+            new EstadoPropiedad { EstadoPropiedadId = 1, NombreEstado = "Activa", Descripcion = "La propiedad está visible y disponible." },
+            new EstadoPropiedad { EstadoPropiedadId = 3, NombreEstado = "Vendida", Descripcion = "La propiedad ha sido vendida y ya no está disponible." },
+            new EstadoPropiedad { EstadoPropiedadId = 4, NombreEstado = "Alquilada", Descripcion = "La propiedad ha sido alquilada y ya no está disponible." },
+            new EstadoPropiedad { EstadoPropiedadId = 6, NombreEstado = "Eliminada", Descripcion = "La propiedad ha sido eliminada del sistema." }
+        );
     }
 }
