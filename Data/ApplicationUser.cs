218e9033a86ff02bcb2cd369e7983c1efaae6bfa
Danielvi <danielvilopez@gmail.com>
2025-08-03 11:27:07 -0400

    Agrega los requerimientos para el campo Nombre y Apellido, además agrega la tabla de Estados

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -6,13 +6,19 @@ namespace InfalibleRealEstate.Data;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required(ErrorMessage = "El nombre es requerido")]
     [PersonalData]
     [StringLength(100)]
     public string Nombre { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "El apellido es requerido")]
     [PersonalData]
     [StringLength(100)]
     public string Apellido { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Debe agregar un estado para el usuario")]
+    public int EstadoUsuarioId { get; set; } = 1; 
+    public virtual EstadoUsuario EstadoUsuario { get; set; }
 
     public virtual ICollection<Propiedad> PropiedadesPublicadas { get; set; } = new List<Propiedad>();
     public virtual ICollection<SuscripcionNoticia> Suscripciones { get; set; } = new List<SuscripcionNoticia>();
