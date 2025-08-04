15584d1771b4d2d0824f9697c5f56ff3f3ae9264
Danielvi <danielvilopez@gmail.com>
2025-08-04 14:33:43 -0400

    Crea la clase Carrito

---
+++ Models/Carrito.cs
@@ -0,0 +1,19 @@
+﻿using InfalibleRealEstate.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InfalibleRealEstate.Models;
+
+public class Carrito
+{
+    [Key]
+    public int CarritoId { get; set; }
+
+    public string? UsuarioId { get; set; }
+
+    [ForeignKey("UsuarioId")]
+    public virtual ApplicationUser? Usuario { get; set; }
+
+    public virtual ICollection<CarritoItem> CarritoItems { get; set; } = new List<CarritoItem>();
+   
+}
