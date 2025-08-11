04b231a0602333f388b426e2c1a0894f4d154932
Danielvi <danielvilopez@gmail.com>
2025-08-11 13:41:53 -0400

    Cambia los nombres de las clases a Plurar

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -18,14 +18,14 @@ public class ApplicationUser : IdentityUser
 
     [Required(ErrorMessage = "Debe agregar un estado para el usuario")]
     public int EstadoUsuarioId { get; set; } = 1; 
-    public virtual EstadoUsuario EstadoUsuario { get; set; }
+    public virtual EstadoUsuarios EstadoUsuario { get; set; }
 
-    public virtual Carrito? Carrito { get; set; }
+    public virtual Carritos? Carrito { get; set; }
 
-    public virtual ICollection<Propiedad> PropiedadesPublicadas { get; set; } = new List<Propiedad>();
-    public virtual ICollection<SuscripcionNoticia> Suscripciones { get; set; } = new List<SuscripcionNoticia>();
-    public virtual ICollection<SolicitudVenta> SolicitudesVenta { get; set; } = new List<SolicitudVenta>();
-    public virtual ICollection<SolicitudUnirse> SolicitudesUnirse { get; set; } = new List<SolicitudUnirse>();
+    public virtual ICollection<Propiedades> PropiedadesPublicadas { get; set; } = new List<Propiedades>();
+    public virtual ICollection<SuscripcionesNoticia> Suscripciones { get; set; } = new List<SuscripcionesNoticia>();
+    public virtual ICollection<SolicitudesVenta> SolicitudesVenta { get; set; } = new List<SolicitudesVenta>();
+    public virtual ICollection<SolicitudesUnirse> SolicitudesUnirse { get; set; } = new List<SolicitudesUnirse>();
     public virtual ICollection<Foros> Foros { get; set; } = new List<Foros>();
     public virtual ICollection<Citas> Citas { get; set; } = new List<Citas>();
 }
