96433829aa9b6e21a05b119ea635e556138c4ee6
Danielvi <danielvilopez@gmail.com>
2025-08-10 21:08:36 -0400

    Crea un archivo js para la animacion al entrar y  para subir el scroll

---
+++ wwwroot/js/animation.js
@@ -0,0 +1,24 @@
+﻿function initializeIntersectionObserver() {
+    const animatedElements = document.querySelectorAll('.animate-slide-up:not(.visible)');
+
+    if (animatedElements.length === 0) return;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    animatedElements.forEach(el => observer.observe(el));
+}
+
+function scrollToTop() {
+    requestAnimationFrame(() => {
+        window.scrollTo(0, 0);
+    });
+}
